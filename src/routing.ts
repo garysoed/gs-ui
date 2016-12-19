6b59d2c3ad39f5d9e857eeaac9c29321e0968dda
Gary Soedarsono <garysoed@gmail.com>
2016-12-18 18:08:14 -0800

    moved routing stuff to gs-ui

---
+++ src/routing.ts
@@ -0,0 +1,6 @@
+export {IRouteFactory} from './routing/interfaces';
+export * from './routing/route';
+export * from './routing/route-service';
+export * from './routing/route-service-events';
+export * from './routing/simple-route-factory';
+
