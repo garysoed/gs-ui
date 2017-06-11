63642d001f4542b65838048a86e587e88ab68dce
Gary Soedarsono <garysoed@gmail.com>
2017-06-11 09:33:56 -0700

    Palette picker for demo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -4,8 +4,6 @@ var base = 'torchred';
 var accent = 'cerulean';
 var contrast = 4.5;
 
-var accentPickerMenu = document.querySelector('.colorPickerMenu.accent');
-var basePickerMenu = document.querySelector('.colorPickerMenu.base');
 
 var palettes = [];
 gs.ui.DefaultPalettes.getNames().forEach(function(name) {
@@ -52,73 +50,87 @@ function onClickAccent(name) {
   updateTheme();
 }
 
-// Create the base color picker.
-for (var r = 0; r < 5; r++) {
-  var rowEl = document.createElement('div');
-  basePickerMenu.appendChild(rowEl);
-  for (var c = 0; c < 5; c++) {
-    var i = r * 5 + c;
-    var palette = palettes[i].palette;
-    var name = palettes[i].name;
+function setupPalettePicker() {
+  var basePickerMenu = document.querySelector('.colorPickerMenu.base');
+  var accentPickerMenu = document.querySelector('.colorPickerMenu.accent');
 
-    var normal = palette;
-    var cellEl = document.createElement('div');
-    cellEl.style.backgroundColor =
-        'rgb(' + [normal.getRed(), normal.getGreen(), normal.getBlue()].join(',') +')';
-    cellEl.classList.add('colorPickerCell');
-    cellEl.classList.add('base');
-    cellEl.title = name;
-    cellEl.setAttribute('data-name', name);
-    cellEl.addEventListener('click', onClickBase.bind(null, name));
+  // Create the base color picker.
+  for (var r = 0; r < 5; r++) {
+    var rowEl = document.createElement('div');
+    basePickerMenu.appendChild(rowEl);
+    for (var c = 0; c < 5; c++) {
+      var i = r * 5 + c;
+      var palette = palettes[i].palette;
+      var name = palettes[i].name;
 
-    var light = palette;
-    var lightCell = document.createElement('div');
-    lightCell.style.backgroundColor =
-        'rgb(' + [light.getRed(), light.getGreen(), light.getBlue()].join(',') + ')';
-    cellEl.appendChild(lightCell);
+      var normal = palette;
+      var cellEl = document.createElement('div');
+      cellEl.style.backgroundColor =
+          'rgb(' + [normal.getRed(), normal.getGreen(), normal.getBlue()].join(',') +')';
+      cellEl.classList.add('colorPickerCell');
+      cellEl.classList.add('base');
+      cellEl.title = name;
+      cellEl.setAttribute('data-name', name);
+      cellEl.addEventListener('click', onClickBase.bind(null, name));
 
-    var dark = palette;
-    var darkCell = document.createElement('div');
-    darkCell.style.backgroundColor =
-        'rgb(' + [dark.getRed(), dark.getGreen(), dark.getBlue()].join(',') + ')';
-    darkCell.classList.add('dark');
-    cellEl.appendChild(darkCell);
+      var light = palette;
+      var lightCell = document.createElement('div');
+      lightCell.style.backgroundColor =
+          'rgb(' + [light.getRed(), light.getGreen(), light.getBlue()].join(',') + ')';
+      cellEl.appendChild(lightCell);
 
-    rowEl.appendChild(cellEl);
+      var dark = palette;
+      var darkCell = document.createElement('div');
+      darkCell.style.backgroundColor =
+          'rgb(' + [dark.getRed(), dark.getGreen(), dark.getBlue()].join(',') + ')';
+      darkCell.classList.add('dark');
+      cellEl.appendChild(darkCell);
+
+      rowEl.appendChild(cellEl);
+    }
   }
+
+  // Create the accent color picker.
+  for (var r = 0; r < 5; r++) {
+    var rowEl = document.createElement('div');
+    accentPickerMenu.appendChild(rowEl);
+    for (var c = 0; c < 5; c++) {
+      var i = r * 5 + c;
+      var palette = palettes[i].palette;
+      var name = palettes[i].name;
+
+      var color = palette;
+      var cellEl = document.createElement('div');
+      cellEl.style.backgroundColor =
+          'rgb(' + [color.getRed(), color.getGreen(), color.getBlue()].join(',') +')';
+      cellEl.classList.add('colorPickerCell');
+      cellEl.classList.add('base');
+      cellEl.setAttribute('data-name', name);
+      cellEl.title = name;
+      cellEl.addEventListener('click', onClickAccent.bind(null, name));
+
+      rowEl.appendChild(cellEl);
+    }
+  }
+
+  // Add logic to the tab.
+  var menuRoot = document.querySelector('.menuRoot');
+  var tab = document.querySelector('.colorPicker gs-horizontal-tab');
+  tab.addEventListener('gse-tab-change', function() {
+    menuRoot.setAttribute('selected-tab', tab.getAttribute('gs-selected-tab'));
+  });
+
+  updateCells();
 }
 
-// Create the accent color picker.
-for (var r = 0; r < 5; r++) {
-  var rowEl = document.createElement('div');
-  accentPickerMenu.appendChild(rowEl);
-  for (var c = 0; c < 5; c++) {
-    var i = r * 5 + c;
-    var palette = palettes[i].palette;
-    var name = palettes[i].name;
-
-    var color = palette;
-    var cellEl = document.createElement('div');
-    cellEl.style.backgroundColor =
-        'rgb(' + [color.getRed(), color.getGreen(), color.getBlue()].join(',') +')';
-    cellEl.classList.add('colorPickerCell');
-    cellEl.classList.add('base');
-    cellEl.setAttribute('data-name', name);
-    cellEl.title = name;
-    cellEl.addEventListener('click', onClickAccent.bind(null, name));
-
-    rowEl.appendChild(cellEl);
-  }
-}
-
-// Add logic to the tab.
-var menuRoot = document.querySelector('.menuRoot');
-var tab = document.querySelector('.colorPicker gs-horizontal-tab');
-tab.addEventListener('gse-tab-change', function() {
-  menuRoot.setAttribute('selected-tab', tab.getAttribute('gs-selected-tab'));
-});
-
-updateCells();
+fetch('demo-palette-picker.html', {method: 'GET'})
+    .then(function(response) {
+      return response.text()
+    })
+    .then(function(textResponse) {
+      document.body.innerHTML += textResponse;
+      setupPalettePicker();
+    });
 
 // Set the templates.
 var templateEl = document.querySelector('#template');
@@ -126,6 +138,7 @@ document.querySelectorAll('section.cell').forEach(function(el) {
   el.innerHTML = templateEl.innerHTML;
 });
 
+// Setup the contrast button.
 var contrastButton = document.querySelector('#contrastButton');
 var contrastDrawer = document.querySelector('#contrastDrawer');
 contrastButton.addEventListener('click', function() {
